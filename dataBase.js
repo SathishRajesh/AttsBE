42acec54a7353bbc3ab470ddd1b819a294d38c73
Sathish R <7122002sathish@gmail.com>
2025-06-27 08:09:36 +0530

    changes

--- dataBase.js
+++ dataBase.js
@@ -7,11 +7,15 @@ const connectDb = async () => {
   try {
 
     const db = await mongoose.connect(process.env.SAN_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-    });
-       logger.info(` MongoDB Connected: ${db}`);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  bufferCommands: false,
+});
+
+logger.info(
+  `MongoDB Connected: host=${db.connection.host}, db=${db.connection.name}`
+);
+
   } catch (error) {
 logger.error(`MongoDB connection failed: ${error.message}`);
     throw error;
