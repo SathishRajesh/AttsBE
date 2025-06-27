d09849ab57241c7dc30b99a4513094ff5d2f2fb7
Sathish R <7122002sathish@gmail.com>
2025-06-27 07:21:28 +0530

    changes

--- dataBase.js
+++ dataBase.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 
-const connectDb = () => {
-  mongoose
-    .connect(process.env.sanDb, {
+let isConnected = false;
+
+const connectDb = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.SAN_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then((success) => {
-      console.log("MongoDb Connected !!!!");
-    })
-    .catch((errorMessage) => {
-      console.log("Error in DataBase file", errorMessage);
+      bufferCommands: false,
     });
+
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB Connected:", db.connection.host);
+  } catch (error) {
+    console.error("Error in DB connection:", error.message);
+    throw new Error("Database connection failed");
+  }
 };
 
 module.exports = connectDb;
