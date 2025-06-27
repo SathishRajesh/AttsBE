b0b64bbb89f223b208047e6f8d3685b6f613b2ac
Sathish R <7122002sathish@gmail.com>
2025-06-27 08:04:25 +0530

    changes

--- dataBase.js
+++ dataBase.js
@@ -2,11 +2,8 @@
 const mongoose = require("mongoose");
 const logger = require("./logger");
 
-let isConnected = false;
 
 const connectDb = async () => {
-  if (isConnected) return;
-
   try {
 
     const db = await mongoose.connect(process.env.SAN_DB, {
@@ -15,7 +12,7 @@ const connectDb = async () => {
       bufferCommands: false,
     });
 
-    isConnected = db.connections[0].readyState === 1;
+     db.connections[0].readyState === 1;
        logger.info(` MongoDB Connected: ${db.connection.host}`);
   } catch (error) {
 logger.error(`MongoDB connection failed: ${error.message}`);
