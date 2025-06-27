413f9168a0b0c2518b0d776543feae394d63e800
Sathish R <7122002sathish@gmail.com>
2025-06-27 08:12:02 +0530

    changes

--- dataBase.js
+++ dataBase.js
@@ -6,15 +6,15 @@ const logger = require("./logger");
 const connectDb = async () => {
   try {
 
-    const db = await mongoose.connect(process.env.SAN_DB, {
+await mongoose.connect(process.env.SAN_DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   bufferCommands: false,
 });
 
-logger.info(
-  `MongoDB Connected: host=${db.connection.host}, db=${db.connection.name}`
-);
+const { host, name } = mongoose.connection;
+
+logger.info(`MongoDB Connected: host=${host}, db=${name}`);
 
   } catch (error) {
 logger.error(`MongoDB connection failed: ${error.message}`);
